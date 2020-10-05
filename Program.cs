108079abd66a7645bd5efa7a43df75b373e05455
Vins <sharmavins23@gmail.com>
2020-10-05 15:39:53 -0400

    🎉 first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
